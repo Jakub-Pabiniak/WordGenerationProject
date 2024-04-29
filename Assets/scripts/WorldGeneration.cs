b838d659ce1a9e44f5b11ce51a22e885f2c2d709
Jakub-Pabiniak <jakubpabiniak2@gmail.com>
2024-04-30 01:22:01 +0200

    River generation change

--- Assets/scripts/WorldGeneration.cs
+++ Assets/scripts/WorldGeneration.cs
@@ -22,14 +22,14 @@ public class WorldGeneration : MonoBehaviour
 {
     public int mapHeight; // Customizable mapHeight
     public int mapWidth; // Customizable mapWidth
-    
+
     public Tilemap ground; //layer for rivers should be on the bottom
     public TileBase[] waterTiles; // list of all water tiles
     public int numberOfRivers; //How many rivers to generate
 
     public Tilemap grass; // layer for grass
     public TileWithSpawnRatios[] grassTilesWithRatios; // list of all grass tiles with their spawnratios
-    
+
 
     public Tilemap shores; // Layer for shores should be above river but under everything else
     public RuleTile shoreTiles; // RuleTile for shores
@@ -51,7 +51,7 @@ public class WorldGeneration : MonoBehaviour
     }
 
     // Function to generate ground from an array of tiles with their spawn ratios
-    public void generate_ground(TileWithSpawnRatios[] ArrayOfTiles, ref int [,] map)
+    public void generate_ground(TileWithSpawnRatios[] ArrayOfTiles, ref int[,] map)
     {
         // Calculate the total sum of spawn ratios for all grass tiles
         float totalSpawnRatio = CalculateTotalSpawnRatio(ArrayOfTiles);
@@ -63,7 +63,7 @@ public class WorldGeneration : MonoBehaviour
             {
                 //Randomly select a tile
                 TileBase selectedTile = GetRandomTile(ArrayOfTiles, totalSpawnRatio);
-                
+
                 // if statement for checking if on coordinates x,y there is no river tile
                 if (map[x, y] == 0) grass.SetTile(new Vector3Int(x, y, 0), selectedTile); // place selected grass tile
             }
@@ -106,13 +106,13 @@ public class WorldGeneration : MonoBehaviour
     {
         int[] indicator = { 0, 0 }; // Create an indicator
         int v = 1;
-        int h = 1; 
+        int h = 1;
         const int riverWidth = 5; // river width (should be odd number)
         TileBase selectedTile;
         var (spoint, epoint) = createPoints(); //starting and ending points
 
         // calculate horiznotal and vertical distance
-        int[] distance = { epoint[0] - spoint[0], epoint[1] - spoint[1]};
+        int[] distance = { epoint[0] - spoint[0], epoint[1] - spoint[1] };
 
         // check if end point is to the left or right of spoint and if needed set aboslute value of a distance
         // h value indicates to which direction will river move (-1 for to the left, (defult) 1 to the right)
@@ -130,14 +130,16 @@ public class WorldGeneration : MonoBehaviour
         }
 
         // Set indicator on the start point
-        indicator[0] = spoint[0]; 
+        indicator[0] = spoint[0];
         indicator[1] = spoint[1];
 
         // randomly choose if river will move horizontaly or verticaly
+        // place river tiles and mark where they are on map[,]
         // if random value x < horiznotal distance it will move horizontaly, else it will move verticaly
         do
         {
             int x = Random.Range(0, distance[0] + distance[1]); //set x to a random value
+            int chance = Random.Range(0, 3);
             if (x < distance[0]) // if random value < horiznotal distance indicator move horizontaly
             {
                 if (isInMap(indicator[0], indicator[1])) //for security check if indicator still in map borders (by deafult it is supposed to always be)
@@ -153,8 +155,8 @@ public class WorldGeneration : MonoBehaviour
                     if (isInMap(indicator[0], indicator[1] + i)) // check if cooridiantes to place tile still in border
                     {
                         selectedTile = waterTiles[Random.Range(0, waterTiles.Length)]; // Randomly select a tile
-                        ground.SetTile(new Vector3Int(indicator[0], indicator[1] + i, 0), selectedTile); // Place tile to the top of the indicator
-                        map[indicator[0], indicator[1] + i] = 1; // mark where river tile is
+                        ground.SetTile(new Vector3Int(indicator[0] , indicator[1] + i, 0), selectedTile); // Place tile to the top of the indicator
+                        map[indicator[0] , indicator[1] + i] = 1; // mark where river tile is
                     }
                     // make river wider to the bottom of indicator
                     if (isInMap(indicator[0], indicator[1] - i)) // check if cooridiantes to place tile still in border
@@ -164,13 +166,20 @@ public class WorldGeneration : MonoBehaviour
                         map[indicator[0], indicator[1] - i] = 1; // mark where river tile is
                     }
                 }
-                indicator[0] += h; // move indicator Heriznotaly closer to epoint
-                distance[0]--; // lower horizontal distance to epoint
-
-
+                if (chance == 0)
+                {
+                    indicator[0] -= h;
+                    distance[0]++;
+                }
+                else
+                {
+                    indicator[0] += h; // move indicator Heriznotaly closer to epoint
+                    distance[0]--; // lower horizontal distance to epoint
+                }
             }
+
             // move indicator Verticaly closer to epoint right and place tiles
-            else // if rando value >= horiznotal distance indicator move Verticaly
+            else // if randomvalue >= horiznotal distance indicator move Verticaly
             {
                 if (isInMap(indicator[0], indicator[1])) //for security check if indicator still in map borders (by deafult it is supposed to always be)
                 {
@@ -178,27 +187,33 @@ public class WorldGeneration : MonoBehaviour
                     ground.SetTile(new Vector3Int(indicator[0], indicator[1], 0), selectedTile); // Place tile on the indicator
                     map[indicator[0], indicator[1]] = 1; // mark where river tile is
                 }
-                //make river wider
-                for (int i = 1; i<= (riverWidth-1) / 2; i++)
+                // make river wider
+                for (int i = 1; i <= (riverWidth - 1) / 2; i++)
                 {
-                    // make river wider to the right of indicator
-                    if (isInMap(indicator[0] + i, indicator[1])) // check if cooridiantes to place tile still in border
+                    // make river wider to the top of indicator
+                    if (isInMap(indicator[0] + i, indicator[1] )) // check if cooridiantes to place tile still in border
                     {
                         selectedTile = waterTiles[Random.Range(0, waterTiles.Length)]; // Randomly select a tile
-                        ground.SetTile(new Vector3Int(indicator[0] + i, indicator[1], 0), selectedTile); // Place tile to the right of the indicator
+                        ground.SetTile(new Vector3Int(indicator[0] + i, indicator[1] , 0), selectedTile); // Place tile to the top of the indicator
                         map[indicator[0] + i, indicator[1] ] = 1; // mark where river tile is
                     }
-                    // make river wider to the left of indicator
-                    if (isInMap(indicator[0] - i, indicator[1])) // check if cooridiantes to place tile still in border
+                    // make river wider to the bottom of indicator
+                    if (isInMap(indicator[0] - i, indicator[1] )) // check if cooridiantes to place tile still in border
                     {
                         selectedTile = waterTiles[Random.Range(0, waterTiles.Length)]; // Randomly select a tile
-                        ground.SetTile(new Vector3Int(indicator[0] - i, indicator[1], 0), selectedTile); // Place tile to the left of the indicator
-                        map[indicator[0] - i, indicator[1]] = 1; // mark where river tile is
+                        ground.SetTile(new Vector3Int(indicator[0] - i, indicator[1], 0), selectedTile); // Place tile to the bottom of the indicator
+                        map[indicator[0] - i, indicator[1] ] = 1; // mark where river tile is
                     }
                 }
-                indicator[1] += v; // move indicator Verticaly closer to epoint
-                distance[1]--; // lower Vertical distance to epoint
-
+                if (chance == 0){
+                    indicator[1] -= v;
+                    distance[1]++;
+                }
+                else
+                {
+                    indicator[1] += v; // move indicator Verticaly closer to epoint
+                    distance[1]--; // lower Vertical distance to epoint
+                }
             }
 
         } while (distance[0] + distance[1] > 0); // do it until reaching and epoint
@@ -207,33 +222,33 @@ public class WorldGeneration : MonoBehaviour
     // Helper function for creating starting and ending points
     private (int[], int[]) createPoints()
     {
-        int[] spoint = {0,0}; //starting point as an array
-        int[] epoint = {0,0}; //ending point as an array
+        int[] spoint = { 0, 0 }; //starting point as an array
+        int[] epoint = { 0, 0 }; //ending point as an array
         int p = Random.Range(0, 2); // generate starting and ending points (0 for points being on Vertical edges of the map, 1 for points being on Horiznotal edges of the map)
         switch (p)
         {
             // Creat starting and ending points on Vertical edges of the map
             case 0:
                 {
-                    spoint = new int[] { Random.Range(4, mapWidth-4), 0 };
-                    epoint = new int[] { Random.Range(4, mapWidth-4), mapHeight};
+                    spoint = new int[] { Random.Range(4, mapWidth - 4), 0 };
+                    epoint = new int[] { Random.Range(4, mapWidth - 4), mapHeight };
                     break;
                 }
             // Creat starting and ending points on Horiznotal edges of the map
             case 1:
                 {
-                    spoint = new int[] { 0, Random.Range(4, mapHeight-4) };
-                    epoint = new int[] { mapWidth, Random.Range(4, mapHeight-4) };
+                    spoint = new int[] { 0, Random.Range(4, mapHeight - 4) };
+                    epoint = new int[] { mapWidth, Random.Range(4, mapHeight - 4) };
                     break;
                 }
-            }
+        }
         return (spoint, epoint);
     }
 
     // Helper function for checking if coordinates x, y are in the map
     private bool isInMap(int x, int y)
     {
-        if (x < 0 || x > mapWidth-1 || y < 0 || y > mapHeight-1) return false;
+        if (x < 0 || x > mapWidth - 1 || y < 0 || y > mapHeight - 1) return false;
         else return true;
     }
 
@@ -244,19 +259,19 @@ public class WorldGeneration : MonoBehaviour
         {
             for (int x = 0; x < mapWidth; x++)
             {
-                if (map[x,y] == 0)
+                if (map[x, y] == 0)
                 {
-                    if (x - 1 > 0 && y - 1 > 0 && map[x -1,y -1] == 1)
+                    if (x - 1 > 0 && y - 1 > 0 && map[x - 1, y - 1] == 1)
                     {
                         if (IsTileEmpty(x, y, 0)) shores.SetTile(new Vector3Int(x, y, 0), shoreTiles);
-                        if (IsTileEmpty(x - 1, y - 1, 0)) shores.SetTile(new Vector3Int(x -1, y -1, 0), shoreTiles);
+                        if (IsTileEmpty(x - 1, y - 1, 0)) shores.SetTile(new Vector3Int(x - 1, y - 1, 0), shoreTiles);
                     }
-                    if (y-1 > 0 && map[x, y -1] == 1)
+                    if (y - 1 > 0 && map[x, y - 1] == 1)
                     {
                         if (IsTileEmpty(x, y, 0)) shores.SetTile(new Vector3Int(x, y, 0), shoreTiles);
-                        if (IsTileEmpty(x, y -1, 0)) shores.SetTile(new Vector3Int(x, y -1, 0), shoreTiles);
+                        if (IsTileEmpty(x, y - 1, 0)) shores.SetTile(new Vector3Int(x, y - 1, 0), shoreTiles);
                     }
-                    if (x + 1 < mapWidth && y-1 > 0 && map[x + 1, y - 1] == 1)
+                    if (x + 1 < mapWidth && y - 1 > 0 && map[x + 1, y - 1] == 1)
                     {
                         if (IsTileEmpty(x, y, 0)) shores.SetTile(new Vector3Int(x, y, 0), shoreTiles);
                         if (IsTileEmpty(x + 1, y - 1, 0)) shores.SetTile(new Vector3Int(x + 1, y - 1, 0), shoreTiles);
@@ -271,14 +286,14 @@ public class WorldGeneration : MonoBehaviour
                         if (IsTileEmpty(x, y, 0)) shores.SetTile(new Vector3Int(x, y, 0), shoreTiles);
                         if (IsTileEmpty(x + 1, y, 0)) shores.SetTile(new Vector3Int(x + 1, y, 0), shoreTiles);
                     }
-                    if (x - 1 > 0 && y + 1 < mapHeight && map[x -1, y + 1] == 1)
+                    if (x - 1 > 0 && y + 1 < mapHeight && map[x - 1, y + 1] == 1)
                     {
                         if (IsTileEmpty(x, y, 0)) shores.SetTile(new Vector3Int(x, y, 0), shoreTiles);
                         if (IsTileEmpty(x - 1, y + 1, 0)) shores.SetTile(new Vector3Int(x - 1, y + 1, 0), shoreTiles);
                     }
                     if (y + 1 < mapHeight && map[x, y + 1] == 1)
                     {
-                        if (IsTileEmpty(x,y,0)) shores.SetTile(new Vector3Int(x, y, 0), shoreTiles);
+                        if (IsTileEmpty(x, y, 0)) shores.SetTile(new Vector3Int(x, y, 0), shoreTiles);
                         if (IsTileEmpty(x, y + 1, 0)) shores.SetTile(new Vector3Int(x, y + 1, 0), shoreTiles);
                     }
                     if (x + 1 < mapWidth && y + 1 < mapHeight && map[x + 1, y + 1] == 1)
@@ -307,20 +322,20 @@ public class WorldGeneration : MonoBehaviour
         {
             // get the size of a decoration tile
             Sprite sprite = (tile.tile as Tile).sprite;
-            float width = sprite.bounds.size.x; // divided by 16 as one cell has 16 pixels
-            float height = sprite.bounds.size.y; // divided by 16 as one cell has 16 pixels
+            float width = sprite.bounds.size.x;
+            float height = sprite.bounds.size.y;
             // try to spawn spawnNumber number of decoration tile
             for (int i = 0; i < tile.spawnNumber; i++)
             {
                 // Choose random position to spawn a tile
-                x = Random.Range(0+(int)(width), mapWidth-(int)(width));
-                y = Random.Range(0+(int)(height), mapHeight-(int)(height));
+                x = Random.Range(0 + (int)(width), mapWidth - (int)(width));
+                y = Random.Range(0 + (int)(height), mapHeight - (int)(height));
 
                 bool shouldplace = true;
                 // check every cell that decoration will occupy, if there is something already there
-                for (int p = 0; p <width; p++)
+                for (int p = 0; p < width; p++)
                 {
-                    for (int q = 0; q <height; q++)
+                    for (int q = 0; q < height; q++)
                     {
                         if (map[x + p, y + q] != 1)
                         {
@@ -331,8 +346,8 @@ public class WorldGeneration : MonoBehaviour
                 }
                 if (shouldplace) // place decoration tile and mark which tile it occupies
                 {
-                    for (int p = 0; p <width; p++)
-                        {
+                    for (int p = 0; p < width; p++)
+                    {
                         for (int q = 0; q < height; q++)
                         {
                             map[x + p, y + q] = 1;
@@ -369,7 +384,8 @@ public class WorldGeneration : MonoBehaviour
             {
                 for (int q = 0; q < height; q++)
                 {
-                    if (isInMap(x + p, y + q)){ //check if still in the map
+                    if (isInMap(x + p, y + q))
+                    { //check if still in the map
                         if (map[x + p, y + q] != 1)
                         {
                             continue;
@@ -402,7 +418,8 @@ public class WorldGeneration : MonoBehaviour
 
             // move the indicator (0 for moveing up, 1 for moveing right, 2 for down, 3 for left)
             int randomMovement = Random.Range(0, 4);
-            switch (randomMovement){
+            switch (randomMovement)
+            {
                 case 0:
                     if (isInMap(x, y + (int)(height))) y += (int)(height);
                     break;
